e18ca99823c4c2bf91ceb33bacbf6e2e7b962772
unknown <sonada903@gmail.com>
2023-06-05 10:04:20 +0900

    사이트제작

---
+++ js/info.js
@@ -0,0 +1,36 @@
+// URL에서 영화 제목을 추출하여 디코딩합니다
+const urlParams = new URLSearchParams(window.location.search);
+const movieTitle = decodeURIComponent(urlParams.get('id'));
+
+// 영화 정보를 가져와서 페이지에 표시합니다
+fetch('movies.json')
+  .then(response => response.json())
+  .then(data => {
+    const movie = data.find(movie => movie.title === movieTitle);
+
+    if (movie) {
+      const posterElement = document.getElementById('movie-poster');
+      const titleElement = document.getElementById('movie-title');
+      const starElement = document.getElementById('movie-star');
+      const directorElement = document.getElementById('movie-director');
+      const castElement = document.getElementById('movie-cast');
+      const releaseYearElement = document.getElementById('movie-release-year');
+      const nationalElement = document.getElementById('movie-nation');
+      const plotElement = document.getElementById('movie-plot');
+      
+
+      posterElement.src = movie.poster;
+      titleElement.textContent = movie.title;
+      starElement.textContent = movie.star;
+      directorElement.textContent = movie.director;
+      castElement.textContent = movie.cast.join(', ');
+      releaseYearElement.textContent = movie.release_year;
+      nationalElement.textContent = movie.nation;
+      plotElement.textContent = movie.plot;
+    } else {
+      console.error('영화 정보를 찾을 수 없습니다:', movieTitle);
+    }
+  })
+  .catch(error => {
+    console.error('영화 정보를 불러오는 도중 오류가 발생했습니다:', error);
+  });
---
+++ js/script.js
@@ -0,0 +1,26 @@
+fetch('movies.json')
+  .then(response => response.json())
+  .then(data => {
+    const movieInfoContainer = document.getElementById('movie-info-container');
+
+    data.forEach(movie => {
+      const movieElement = document.createElement('div');
+      movieElement.classList.add('movie-info');
+      movieElement.innerHTML = `
+        <img src="${movie.poster}" class="movie-poster">
+        <h2>${movie.title}</h2>
+        <p>${movie.release_year} · ${movie.nation}</p>
+        <p>평점 ★ ${movie.star}</p>
+      `;
+
+      movieElement.addEventListener('click', () => {
+        // 해당 영화의 정보가 담긴 HTML 페이지로 이동
+        window.location.href = 'movie.html?id=' + encodeURIComponent(movie.title);
+      });
+
+      movieInfoContainer.appendChild(movieElement);
+    });
+  })
+  .catch(error => {
+    console.error('영화 정보를 불러오는 도중 오류가 발생했습니다:', error);
+  });
