d3ddd571c44c2380d51df90b4a21da3318e92cd1
unknown <sonada903@gmail.com>
2023-06-11 16:54:20 +0900

    영화 오버레이 적용

--- js/script.js
+++ js/script.js
@@ -6,12 +6,31 @@ fetch('movies.json')
     data.forEach(movie => {
       const movieElement = document.createElement('div');
       movieElement.classList.add('movie-info');
-      movieElement.innerHTML = `
-        <img src="${movie.poster}" class="movie-poster" loading='lazy'>
+      
+      const posterContainer = document.createElement('div');
+      posterContainer.classList.add('poster-container');
+
+      const moviePoster = document.createElement('img');
+      moviePoster.src = movie.poster;
+      moviePoster.classList.add('movie-poster');
+      moviePoster.loading = 'lazy';
+
+      const movieTitle = document.createElement('h2');
+      movieTitle.classList.add('movie-title');
+      movieTitle.textContent = movie.title;
+
+      const overlay = document.createElement('div');
+      overlay.classList.add('overlay');
+      overlay.innerHTML = `
         <h2>${movie.title}</h2>
         <p>${movie.release_year} · ${movie.nation}</p>
         <p>평점 ★ ${movie.star}</p>
       `;
+
+      posterContainer.appendChild(moviePoster);
+      posterContainer.appendChild(movieTitle);
+      posterContainer.appendChild(overlay);
+      movieElement.appendChild(posterContainer);
 
       movieElement.addEventListener('click', () => {
         // 해당 영화의 정보가 담긴 HTML 페이지로 이동
@@ -24,3 +43,5 @@ fetch('movies.json')
   .catch(error => {
     console.error('영화 정보를 불러오는 도중 오류가 발생했습니다:', error);
   });
+
+  
